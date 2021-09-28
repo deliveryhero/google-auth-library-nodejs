6f2aa489b738d7d4be5f71d9670376d624142239
Mohamed Azouz <mohamedaliazouz@gmail.com>
2021-09-28 18:04:16 +0200

    DAT-8016 use accesstoken provider

--- src/auth/singletokenclient.ts
+++ src/auth/singletokenclient.ts
@@ -1,12 +1,35 @@
-import {OAuth2Client} from './oauth2client';
+import {GetTokenResponse, OAuth2Client} from './oauth2client';
+import {Credentials} from './credentials';
+
+export interface AccessToken {
+    accessToken: string;
+    expiryDate: number;
+}
+
+export interface IAccessTokenProvider {
+    fetchAccessToken: () => Promise<AccessToken>;
+}
 
 export class SingleTokenClient extends OAuth2Client {
-  constructor(accessToken: string) {
-    super();
-    this.setCredentials({access_token: accessToken});
-  }
 
-  fromJSON(): void {
-    throw new Error('Method not implemented');
-  }
+    constructor(private readonly accessTokenProvider: IAccessTokenProvider) {
+        super();
+    }
+
+    public async getAndUpdateCredentials(): Promise<Credentials> {
+        const accessToken: AccessToken = await this.accessTokenProvider.fetchAccessToken();
+        this.setCredentials({
+            access_token: accessToken.accessToken,
+            expiry_date: accessToken.expiryDate
+        });
+        return this.credentials;
+    }
+
+    protected async refreshToken(refreshToken?: string | null): Promise<GetTokenResponse> {
+        const credentials = await this.getAndUpdateCredentials();
+        return {
+            tokens: credentials,
+            res: null
+        }
+    }
 }
